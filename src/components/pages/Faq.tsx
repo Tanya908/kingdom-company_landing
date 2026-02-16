ea2759d9b2f3b0bb33bdcd54c369c02a492d9280
Tetiana <tanya67722@gmail.com>
2026-02-16 18:22:23 +0200

    Extract FAQ data from Component

--- src/components/pages/Faq.tsx
+++ src/components/pages/Faq.tsx
@@ -1,23 +1,9 @@
 import Circles from "@/assets/icons/About.svg"
 import Close from "@/assets/icons/Close.svg?react";
 import Open from "@/assets/icons/Open.svg";
+import {faqs} from "../../data/faqs"
 import {useState} from "react";
 import { motion, AnimatePresence } from "framer-motion";
-
-type Faq = {
-    title: string;
-    description: string;
-}
-
-export const Faqs: Faq[] =[
-    { title:"Who is this event for?", description:"Kingdom & Company is for entrepreneurs, professionals, creatives, and emerging leaders who want to align business growth with Kingdom purpose. Whether you're running a startup, leading a team, or simply seeking clarity in your calling — this room is for you."},
-    { title:"Can I attend virtually?", description:"At this time, Kingdom & Company is designed as an in-person experience, with an intentional focus on community, connection, and real-world interaction. However, recordings of all main sessions will be available on-demand for registered attendees after the event."},
-    { title:"Is there a youth discount?", description:"Yes — we offer a special “Student Registration” rate of $99 for participants under the age of 20. This registration includes the same access rights and benefits as all other registration levels."},
-    { title:"Are meals included in the ticket price?", description:"Yes. Lunch and dinner will be provided for all participants on Friday. Light breakfasts (e.g., coffee and pastries) will also be available for purchase at the event venue on Friday morning. No lunch will be provided on Thursday."},
-    { title:"Will the sessions be recorded?", description:"Yes — all keynotes, panels, and main sessions will be professionally recorded and made available on-demand to registered attendees after the event."},
-    { title:"Is there nearby accommodation?", description:"The nearest (and only) hotel in town is Lakeside Stays, partners of K//C. There's also many local airbnb recommendations. To find out more, email us."},
-]
-
 
 const Faq = () => {
     const [openIndex, setOpenIndex] = useState<number | null>(0);
@@ -36,12 +22,12 @@ const Faq = () => {
             </div>
 
             <div>
-                {Faqs.map((faq, index) => {
+                {faqs.map((faq, index) => {
                     const isOpen = openIndex === index;
 
                     return (
                         <div
-                            key={faq.title}
+                            key={index}
                             className={`py-6 px-4 border-l-2 mb-8 transition-all duration-300 ${
                                 isOpen
                                     ? "border-[var(--color-orange)]"
---
+++ src/data/faqs.ts
@@ -0,0 +1,10 @@
+import type {Faq} from "../types/faq.ts";
+
+export const faqs: Faq[] =[
+    { title:"Who is this event for?", description:"Kingdom & Company is for entrepreneurs, professionals, creatives, and emerging leaders who want to align business growth with Kingdom purpose. Whether you're running a startup, leading a team, or simply seeking clarity in your calling — this room is for you."},
+    { title:"Can I attend virtually?", description:"At this time, Kingdom & Company is designed as an in-person experience, with an intentional focus on community, connection, and real-world interaction. However, recordings of all main sessions will be available on-demand for registered attendees after the event."},
+    { title:"Is there a youth discount?", description:"Yes — we offer a special “Student Registration” rate of $99 for participants under the age of 20. This registration includes the same access rights and benefits as all other registration levels."},
+    { title:"Are meals included in the ticket price?", description:"Yes. Lunch and dinner will be provided for all participants on Friday. Light breakfasts (e.g., coffee and pastries) will also be available for purchase at the event venue on Friday morning. No lunch will be provided on Thursday."},
+    { title:"Will the sessions be recorded?", description:"Yes — all keynotes, panels, and main sessions will be professionally recorded and made available on-demand to registered attendees after the event."},
+    { title:"Is there nearby accommodation?", description:"The nearest (and only) hotel in town is Lakeside Stays, partners of K//C. There's also many local airbnb recommendations. To find out more, email us."},
+]
